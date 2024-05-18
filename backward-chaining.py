5f3610b3fa9bee2c3395de390924539dac0c540d
Hồ Thanh Tùng <105987148+Vantangvantang@users.noreply.github.com>
2024-05-19 01:21:20 +0700

    hi

---
+++ backward-chaining.py
@@ -0,0 +1,68 @@
+def read_rules(filename):
+    with open(filename, 'r') as file:
+        return [line.replace('&&', '^').strip() for line in file]
+
+def show_rules(rules):
+    for i, rule in enumerate(rules, start=1):
+        print(f"Rule {i}: {rule}")
+
+def split_rule(rule):
+    parts = rule.split(" => ")
+    condition = parts[0]
+    result = parts[1]
+    condition = condition.replace('||', '').replace('^', '').replace('(', '').replace(')', '')
+    splited_condition_list = condition.split()
+    return condition, result, splited_condition_list
+
+def excute_backward_chaining(rules, goal_list, known_facts, num_back, VET):
+    while goal_list:
+        is_goal_list_changed = False            
+        print(f'============== current goal_list: {goal_list} ==============')
+        for goal in goal_list:
+            VET.append(goal)
+            for rule in rules:
+                print(f'***** {rule} *****')
+                condition, result, splited_condition_list = split_rule(rule)
+                if goal == result:
+                    print(f'{goal} = {result}')
+                    print('- splited_condition_list: ',splited_condition_list)
+                    newsplited_condition_list = []
+                    for item in splited_condition_list:
+                        print('  + item: ', item)
+                        if item in known_facts:
+                            print(f'  + remove {item}')
+                        else:
+                            print(f'  + {item} not in known_facts: ')
+                            newsplited_condition_list.append(item)
+                    print("---------------------")
+                    goal_list.remove(goal)
+                    goal_list.extend(newsplited_condition_list)
+                    is_goal_list_changed = True
+                    continue
+                else:
+                    print(f'{goal} != {result}')
+            if is_goal_list_changed:
+                break
+        if not is_goal_list_changed:
+            break
+        else:
+            num_back += 1
+        print(f"=> goal_list= {goal_list}\n   num_back= {num_back}\n   VET= {VET}")
+    if goal_list:
+        return False
+    if not goal_list:
+        return True        
+
+rules = read_rules("rules.txt")
+show_rules(rules)
+
+num_back = 0
+goal = "watermelon"
+goal_list = [goal]
+VET = []
+known_facts = ["long", "round", "oblong", "diameterabove4", "diameterbelow4", "smooth", "rough", "green", "yellow", "tan", "orange", "red", "purple", "countseedabove1", "countseedbelow1"]
+
+if(excute_backward_chaining(rules, goal_list, known_facts, num_back, VET) == True):
+    print(f"{goal} true")
+else:
+    print(f"{goal} false")
